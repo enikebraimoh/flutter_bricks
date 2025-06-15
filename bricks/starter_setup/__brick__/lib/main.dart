bd7001914db77625b2df50044fff490a551d1a9a
enikebraimoh <richardbraimoh@gmail.com>
2025-06-15 10:34:43 +0100

    Refactor starter_setup brick: update import paths to use dynamic naming, remove generated router file, and improve error handling and app initialization in main.dart for better readability and maintainability.

--- bricks/starter_setup/__brick__/lib/main.dart
+++ bricks/starter_setup/__brick__/lib/main.dart
@@ -1,19 +1,20 @@
-import 'package:Tickets_by_Selar/models/auth_user.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
+import 'package:{{name.snakeCase()}}/shared/models/auth_user.dart';
+import 'package:{{name.snakeCase()}}/shared/router/router.dart';
 import 'config/app_configs.dart';
 import 'config/app_startup.dart';
 import 'generated/l10n.dart';
-import 'router/router.dart';
 import 'theme/light.dart';
 
 void handleError(FlutterErrorDetails errorDetails) async {
-  Sentry.captureException(errorDetails.exception,
-      stackTrace: errorDetails.stack);
+  Sentry.captureException(
+    errorDetails.exception,
+    stackTrace: errorDetails.stack,
+  );
 }
 
 void main() async {
@@ -21,13 +22,10 @@ void main() async {
   await AppStartUp().setUp();
   if (kReleaseMode) {
     // Only initialize Sentry in release mode
-    await SentryFlutter.init(
-      (options) {
-        options.dsn = AppConstants.sentryDSN;
-        options.tracesSampleRate = 0.1;
-      },
-      appRunner: () => initializeApp(),
-    );
+    await SentryFlutter.init((options) {
+      options.dsn = AppConstants.sentryDSN;
+      options.tracesSampleRate = 0.1;
+    }, appRunner: () => initializeApp());
   } else {
     // In debug mode, just run the app directly
     initializeApp();
@@ -48,12 +46,7 @@ void initializeApp() async {
       var user = getIt<UserModel>();
       debugPrint("Setting user on Sentry");
       Sentry.configureScope((scope) {
-        scope.setUser(
-          SentryUser(
-            name: user.firstName,
-            email: user.email,
-          ),
-        );
+        scope.setUser(SentryUser(name: user.firstName, email: user.email));
       });
     }
   }
@@ -62,36 +55,33 @@ void initializeApp() async {
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
+  MyApp();
 
   final _appRouter = AppRouter();
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(375, 812),
-      builder: (BuildContext context, child) => GestureDetector(
-        onTap: () {
-          FocusScopeNode currentFocus = FocusScope.of(context);
-          if (!currentFocus.hasPrimaryFocus &&
-              currentFocus.focusedChild != null) {
-            currentFocus.focusedChild?.unfocus();
-          }
-        },
-        child: MaterialApp.router(
-          routerConfig: _appRouter.config(),
-          localizationsDelegates: const [
-            S.delegate,
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
-          ],
-          supportedLocales: S.delegate.supportedLocales,
-          debugShowCheckedModeBanner: false,
-          title: 'Tickets by Selar',
-          themeMode: ThemeMode.light,
-          theme: lightTheme,
-        ),
+    return GestureDetector(
+      onTap: () {
+        FocusScopeNode currentFocus = FocusScope.of(context);
+        if (!currentFocus.hasPrimaryFocus &&
+            currentFocus.focusedChild != null) {
+          currentFocus.focusedChild?.unfocus();
+        }
+      },
+      child: MaterialApp.router(
+        routerConfig: _appRouter.config(),
+        localizationsDelegates: const [
+          S.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: S.delegate.supportedLocales,
+        debugShowCheckedModeBanner: false,
+        title: '{{name.pascalCase()}}',
+        themeMode: ThemeMode.light,
+        theme: AppTheme.lightTheme,
       ),
     );
   }
